28d648e225160b1d0257457d7c774446be0431c2
Gerard-Dylan <D.marcgerard@gmail.com>
2025-03-19 16:41:47 +0100

    FIX de la dynamisation 'show/hide' de la fenetre déroulante du filtre à la selection du bouton 'appliquer'

--- Javascript.js
+++ Javascript.js
@@ -1,13 +1,32 @@
+const dropdown = document.querySelector(".dropdown");
+const button = dropdown.querySelector("button");
+
+button.addEventListener("click", function (event) {
+    event.stopPropagation();
+    dropdown.classList.toggle("show");
+});
+
+document.addEventListener("click", function (event) {
+    if (!dropdown.contains(event.target)) {
+        dropdown.classList.remove("show");
+    }
+});
+
+
 function applyFilter() {
     const isVegan = document.getElementById('vegan').checked;
     const foodType = document.getElementById('foodType').value;
     const sortOrder = document.getElementById('sortOrder').value;
 
+
     if (isVegan && foodType !== "amazonie") {
         console.log("Repas vegan Amazonien seulement.");
         return;
+
     }
 
     const filterData = { isVegan, foodType, sortOrder };
     console.log("Filtre sélectionné:", filterData);
+    dropdown.classList.remove("show");
+
 }

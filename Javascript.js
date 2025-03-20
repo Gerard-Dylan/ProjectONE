1e4deb4a7f4ca40aca54761c655394c70599814b
K-Smagghe <kevin.smagghe.pro@gmail.com>
2025-03-20 17:03:30 +0100

    add menuburger

--- Javascript.js
+++ Javascript.js
@@ -29,3 +29,24 @@ function applyFilter() {
     dropdown.classList.remove("show");
 
 }
+
+/* Navigation Menu burger */
+
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    sidenav.classList.remove("active");
+}
+
+/
